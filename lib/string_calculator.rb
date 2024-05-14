ceb7fbe538d8cf658df6d0a9e11d52bf462e9a4e
debanshu-majumdar18 <debanshu.majumdar@m360research.com>
2024-05-14 18:41:48 +0530

    [logic added for custom delimiter, invalid input and negative numbers] - Passed

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,8 +3,24 @@ class StringCalculator
     if input.empty?
       0
     else
-      numbers = input.split(",").map { |num| num.to_i }
-      sum = numbers.sum
+      delimiter = ","
+      if input.start_with?("//")
+        delimiter = input[2]
+        input = input[4..-1]
+      end
+
+      if input.include?(",\n")
+        raise ArgumentError, "Invalid input: '#{input}'"
+      end
+
+      numbers = input.split(/#{delimiter}|\n/).map(&:to_i)
+
+      negatives = numbers.select { |num| num < 0 }
+      if negatives.any?
+        raise "Negative numbers not allowed: #{negatives.join(",")}"
+      end
+
+      sum = numbers.reject { |num| num < 0 }.sum
     end
   end
 end
