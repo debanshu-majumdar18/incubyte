ad8076815675adda6f74e2a5af3371b329584f85
debanshu-majumdar18 <debanshu.majumdar@m360research.com>
2024-05-14 18:37:56 +0530

    [Tests for new lines as delims, custom delims and invalid input] - failed

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,31 +7,55 @@ describe StringCalculator do
         expect(StringCalculator.add("")).to eq(0)
       end
     end
-  end
 
-  describe ".add" do
-    context "given '4'" do
-      it "returns 4" do
-        expect(StringCalculator.add("4")).to eql(4)
+    context "given a single number" do
+      context "example: '4'" do
+        it "returns 4" do
+          expect(StringCalculator.add("4")).to eql(4)
+        end
+      end
+
+      context "example: '10'" do
+        it "returns 10" do
+          expect(StringCalculator.add("10")).to eql(10)
+        end
       end
     end
 
-    context "given '10'" do
-      it "returns 10" do
-        expect(StringCalculator.add("10")).to eql(10)
-      end
-    end
-
-    context "two numbers" do
-      context "given '4,4'" do
+    context "given two numbers" do
+      context "example: '4,4'" do
         it "returns 8" do
           expect(StringCalculator.add("4,4")).to eql(8)
         end
       end
 
-      context "given '25,75'" do
+      context "example: '25,75'" do
         it "returns 100" do
           expect(StringCalculator.add("25,75")).to eql(100)
+        end
+      end
+
+      context "with newlines" do
+        context "example: '1\n2,3'" do
+          it "returns 6" do
+            expect(StringCalculator.add("1\n2,3")).to eql(6)
+          end
+        end
+      end
+    end
+
+    context "handling custom delimiters" do
+      context "example: '//;\n1;2'" do
+        it "returns 3" do
+          expect(StringCalculator.add("//;\n1;2")).to eql(3)
+        end
+      end
+    end
+
+    context "invalid input with a comma followed by newline" do
+      context "example: '1,\n'" do
+        it "raises an ArgumentError" do
+          expect { StringCalculator.add("1,\n") }.to raise_error(ArgumentError)
         end
       end
     end
